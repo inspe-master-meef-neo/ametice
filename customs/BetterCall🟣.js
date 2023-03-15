abdef0e65872aa752d131473f6ba0c1bd1983510
prb404 <64775485+prb404@users.noreply.github.com>
2023-03-15 13:01:40 +0100

    Update BetterCall🟣.js

--- customs/BetterCall🟣.js
+++ customs/BetterCall🟣.js
@@ -18,6 +18,11 @@ function NEOCustoms(filename, filetype){
  }
 }
 
+// external CSS
+//loadjscssfilesmith("https://url.com/.../customStyleSheet.css", "css") ////dynamically load and add this .css file
+NEOCustoms("https://inspe-master-meef-neo.github.io/ametice/customs/BetterCall🟣.css", "css")
+NEOCustoms("https://inspe-master-meef-neo.github.io/ametice/customs/mdb/css/mdb.min.css", "css")
+
 // external JS
 // NEOCustoms("https://url.com/.../customJavaScript.js", "js") //dynamically load and add this .js file
 // NEOCustoms("https://cdn.logwork.com/widget/countdown.js", "js")
@@ -25,7 +30,3 @@ NEOCustoms("https://inspe-master-meef-neo.github.io/ametice/customs/mdb/js/mdb.m
 NEOCustoms("https://inspe-master-meef-neo.github.io/ametice/customs/extras/globlol.js", "js")
 
 
-// external CSS
-//loadjscssfilesmith("https://url.com/.../customStyleSheet.css", "css") ////dynamically load and add this .css file
-NEOCustoms("https://inspe-master-meef-neo.github.io/ametice/customs/BetterCall🟣.css", "css")
-NEOCustoms("https://inspe-master-meef-neo.github.io/ametice/customs/mdb/css/mdb.min.css", "css")
